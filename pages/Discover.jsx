3b28a78fab0c0eb35c209e5f407d3a45e250fe2d
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-16 16:17:59 +0200

    Restart on podcast app, get the template of file layout
    - install
     - react-router
     - tailwind CSS
     - axios
     - react-icons
     - react-redux
     - swiper
     - reactjs/toolkit
     - install react

---
+++ pages/Discover.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import {Error, Loader, Podcard} from '../components'
+import {genres} from '../assets/constants'
+const Discover = () => <div>Discover</div>;
+
+export default Discover;
