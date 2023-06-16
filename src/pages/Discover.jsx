baf8e501f0993ac6830acc48b56f6216dd4de1ec
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-16 19:37:43 +0200

    add styling to the layout the the div tags
    - add styling to h2 div
    - add styling tot he layout of the select tag
    - make sure that they are responsive

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,7 +5,11 @@ const Discover = () => {
 
     return(
         
-        <div >
+        <div className="flex flex-col">
+            <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mv-10">
+                <h2 className="font-bold text-3xl text-[#e9e2e7] text-left">DISCOVER</h2>
+                <select></select>
+            </div>
             Discover
         </div>
     )
