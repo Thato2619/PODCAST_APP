a754d72dc3099726c4c793ec4169eb69f95ba815
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-16 21:52:47 +0200

    add prooperty in the first select tag, add styling to the option tags
    map over all the category in the option tag
      - Don't forget to add key which is the id of category

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,12 +3,20 @@ import {Error, Loader} from '../components'
 import {allCategories} from '../assets/genre/allCategories'
 const Discover = () => {
 
+    const categoryTitle = 'Comedy'
+
     return(
         
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mv-10">
-                <h2 className="font-bold text-3xl text-[#e9e2e7] text-left">DISCOVER</h2>
-                <select></select>
+                <h2 className="font-bold text-3xl text-[#e9e2e7] text-left">DISCOVER {categoryTitle}</h2>
+                <select
+                    onChange={() => {}}
+                    value=""
+                    className="bg-[#090708] text-[#aa8da2] p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+                >
+                    {allCategories.map((category) => <option key={category.id} value={category.id}>{category.title}</option>)}
+                </select>
             </div>
             Discover
         </div>
